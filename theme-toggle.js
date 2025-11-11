6d489a5641d413c11af9b01cfd1a447629accbc2
DGrothe-PhD <dag_fs@t-online.de>
2025-11-11 21:23:58 +0100

    wooden appearance and nicer colouring

--- theme-toggle.js
+++ theme-toggle.js
@@ -2,6 +2,8 @@ const toggleBtn = document.getElementById('theme-toggle');
 const sunIcon = document.getElementById('sun-icon');
 const moonIcon = document.getElementById('moon-icon');
 const body = document.body;
+const header = document.body.querySelector('header');
+//const nav = document.body.querySelector('nav');
 
 // Prüfe gespeicherte Einstellung
 let themeStorage = null;
@@ -12,12 +14,16 @@ try {
 }
 if (themeStorage && localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark-mode');
+  header.classList.add('dark-mode');
+  //nav.classList.add('dark-mode');
   sunIcon.classList.add('hidden');
   moonIcon.classList.remove('hidden');
 }
 
 toggleBtn.addEventListener('click', () => {
   const isDark = body.classList.toggle('dark-mode');
+  header.classList.toggle('dark-mode');
+  //nav.classList.toggle('dark-mode');
   sunIcon.classList.toggle('hidden', isDark);
   moonIcon.classList.toggle('hidden', !isDark);
   if (themeStorage) {
