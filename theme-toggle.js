730d0abb1c5f359fcbf80ea50dafeb7be7a5f188
DGrothe-PhD <64636863+DGrothe-PhD@users.noreply.github.com>
2026-04-01 17:10:01 +0200

    Added svg stats image to close #12 along with some wording amendments

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.getElementById('theme-toggle');
 const sunIcon = document.getElementById('sun-icon');
 const moonIcon = document.getElementById('moon-icon');
+const statsDark = document.getElementById('stats-dark');
+const statsLight = document.getElementById('stats-light');
 const body = document.body;
 const header = document.body.querySelector('header');
 //const nav = document.body.querySelector('nav');
@@ -26,6 +28,8 @@ if (toggleBtn) {
     if (header) header.classList.toggle('dark-mode');
     if (sunIcon) sunIcon.classList.toggle('hidden', isDark);
     if (moonIcon) moonIcon.classList.toggle('hidden', !isDark);
+    if (statsLight) statsLight.classList.toggle('hidden', isDark);
+    if (statsDark) statsDark.classList.toggle('hidden', !isDark);
     if (themeStorage) {
       themeStorage.setItem('theme', isDark ? 'dark' : 'light');
     }
